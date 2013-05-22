54db56bda5bdd2e6661da904339e1eeee176e286
Geoff Hulette <ghulette@gmail.com>
2013-05-21 22:10:46 -0700

    Update

--- radix.ml
+++ radix.ml
@@ -1,11 +1,48 @@
 type 'a t =
-  | Node of (char list * 'a t) list
+  | Node of ((char list) * ('a t)) list
   | Leaf of 'a
+
+let explode s = 
+  let cs = ref [] in
+  for i = 0 to (String.length s - 1) do
+    cs := s.[i] :: !cs
+  done; List.rev (!cs)
+
+let replace k1 k2 v = function
+  | [] -> raise Not_found
+  | (x::xs) when x = k1 -> 
 
 let empty = Node []
 
-let rec prefix_of xs ys =
-  match (xs,ys) with
-  | (x::xs',y::ys') when x = y -> prefix_of xs' ys'
-  | ([],ys) -> Some ys
-  | (_,_) -> None
+let rec prefix xs ys =
+  match xs,ys with
+  | x::xs',y::ys' when x = y -> prefix xs' ys'
+  | _,[] -> Some xs
+  | _,_ -> None
+
+let rec descend k = function
+  | [] -> None
+  | ((lbl,r)::edgs) -> 
+    match prefix k lbl with
+    | None -> descend k edgs
+    | Some k' -> Some (lbl,k',r)
+
+let rec lookup k = function
+  | Leaf x -> if k = [] then Some x else None
+  | Node edgs -> 
+    match descend k edgs with
+    | None -> None
+    | Some (_,k',r) -> lookup k' r
+
+let rec insert k v = function
+  | Node edges -> 
+    begin match descend k edges with
+    | None -> Node [(k,Leaf v)]
+    | Some (pre,rest,t) -> Node (replace edges pre (insert k' v t))
+    end
+  | Leaf x ->
+    begin match k with
+    | [] -> Leaf v
+    | _ -> Node [([],Leaf x);(k,Leaf v)]
+    end
+  
